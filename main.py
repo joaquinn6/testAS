0c9b63f1314af2a1243944f02f40388235e135f2
Joaquin <joaquin.nunez@hotaka.io>
2024-05-15 05:16:14 -0600

    bar value:percent, initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+'''importaciones'''
+from utils import string_to_json, valid_format, higher_absolute_value, percent_of
+
+
+def generate_bar_code():
+  '''Función que espera una entrada json string de consola 
+  he imprime un gráfico de barras igualmente en consola'''
+  print('Ingrese un json valido:')
+  string_json = input()
+  try:
+    data = string_to_json(string_json)
+    if not valid_format(data):
+      print('Formato de datos incorrecto')
+      return
+
+    bars = make_tuples_percents(data)
+    print(bars)
+  except Exception as exc:
+    print(exc.args[0])
+
+
+def make_tuples_percents(data: dict) -> list:
+  '''Recibe un objeto de clave:valor donde valor es siempre un float y 
+  lo transforma en una lista de tuplas con el valor convertido en porcentaje'''
+  bars = []
+  biggest_value = higher_absolute_value(data)
+  for item in data.items():
+    bar = (item[0], percent_of(item[1], biggest_value))
+    bars.append(bar)
+  return bars
+
+
+generate_bar_code()
---
+++ utils.py
@@ -0,0 +1,58 @@
+'''importaciones'''
+import json
+
+
+def string_to_json(json_string: str) -> dict:
+  '''Recibe un json en formato string y lo transforma en un diccionario
+  Argumentos:
+    json_string: Json en formato string
+  Devuelve:
+    Un diccionario a partir del json_string
+  '''
+  try:
+    json_object = json.loads(json_string)
+    return json_object
+  except Exception as exc:
+    raise Exception(f'Json invalido: {exc.args[0]}') from exc
+
+
+def valid_format(data: dict) -> bool:
+  '''Valida si todos los valores de un diccionario son floats o pueden ser convertidos a floats.
+  Argumentos:
+    data: El diccionario que se desea validar.
+  Devuelve:
+    True si todos los valores del diccionario son floats o pueden ser convertidos a floats
+    False en caso contrario.
+  '''
+  for item in data.items():
+    if isinstance(data[item[0]], float):
+      continue
+    try:
+      data[item[0]] = float(item[1])
+    except ValueError:
+      return False
+  return True
+
+
+def higher_absolute_value(data: dict) -> float:
+  '''Calcula el valor absoluto más alto de un objeto
+  Argumentos:
+    data: diccionario clave:valor donde valor sera siempre un float
+  Devuelve:
+    Un numero flotante que representa el valor absoluto más alto
+  '''
+  valor_absoluto_maximo = max(abs(item[1]) for item in data.items())
+  return valor_absoluto_maximo
+
+
+def percent_of(value: float, percent_100: float) -> float:
+  '''Calcula el porcentaje de un valor en relación a otro valor.
+  Argumentos:
+    value: El valor del que se desea calcular el porcentaje. Debe ser un número flotante.
+    percent_100: El valor que representa el 100%. Debe ser un número flotante.
+  Devuelve:
+    Un número flotante que representa el porcentaje del `value` en relación al `percent_100`
+    redondeado a 2 decimales.
+  '''
+  porcentaje = value * 100 / percent_100
+  return round(porcentaje, 2)
